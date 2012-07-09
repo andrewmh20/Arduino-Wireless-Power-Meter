71108f242fcbe7309450a8fe524b797c5d53266b
andrewmh20 <andrewmh20@nyc.rr.com>
2012-07-09 12:46:59 -0400

    First workable code for listening to Xbee Serial

---
+++ TAW.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+
+uint8_t ssRX = 8;
+uint8_t ssTX =9;
+
+SoftwareSerial xbee(ssRX, ssTX);
+
+
+void setup() {
+Serial.begin(9600);
+xbee.begin(9600);
+}
+
+void loop()
+{
+
+  int N = 12;
+  int a[N];
+  
+  int i = 0;
+  char c;
+  
+  while (i<N) {
+   
+    c = xbee.read();
+    a[i] = c;
+    i = c++;
+  }
+ 
+Serial.println(a[i]);
+
+delay(10000);
+
+}
+
+
