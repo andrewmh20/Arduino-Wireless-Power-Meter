e1f169057d2ec504435aa32a0c0ff72307861315
andrewmh20 <andrewmh20@nyc.rr.com>
2012-07-10 09:54:14 -0400

    Added another xbee.read();
    Now gets reasonable serial data

--- TAWv2_Parse_Serial/TAWv2_Parse_Serial.ino
+++ TAWv2_Parse_Serial/TAWv2_Parse_Serial.ino
@@ -19,9 +19,13 @@ void loop()
   char c = xbee.read();
 //If the first byte is 7E HEX then read the next 2 bytes
     if(c == 0x7E){
-      
+    Serial.println("SOH");
+while(!xbee.available()); 
       int lengthMSB = xbee.read();
+
+while(!xbee.available()); 
       int lengthLSB = xbee.read();
+
 //And use them to calculate length of [???the rest of???] incoming trasmission
       int length = (lengthLSB + (lengthMSB << 8)) + 1;
 //Print the length to Serial Monitor (For debugging)      
@@ -32,24 +36,26 @@ void loop()
 //While i is less than that length, read 1 byte from Xbee
 //and save it to the array
   while (i<length) {
-  a[i] = c;
+    while(!xbee.available());
+    c = xbee.read();
+    a[i] = c;
 //then increment i, so the next byte is stored in the next location in the array
-  i = i++;
+    i++;
 }
 //Print each byte of the array
-  for (i=0; i < length; i++){
-    Serial.println(a[i]);
-                            }
- 
+  for (i=0; i<length; i++){
+    Serial.println(a[i], HEX);
+  }
+  
  //***********Inside "if loop" put instructions for taking bytes from array
  //and putting them into a new int array,
  //Do I need to find 0x83 and treat as new packet, or can I just use array a[length]
  //???*******************
   }
  //If 7E was not found, then no packet was received   
-    else {
-      Serial.println("NO DATA RECEIVED");
-}
+//    else {
+//      Serial.println("NO DATA RECEIVED");
+//}
 
 
 
